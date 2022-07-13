aeabbbcb62f7cba18a1cee40085d79a18a528737
triabi931 <41240299+triabi931@users.noreply.github.com>
2022-07-13 11:10:05 +0700

    13 Juli 2022

---
+++ viewpdf.php
@@ -0,0 +1,15 @@
+<style>
+body {
+    margin: 0;            /* Reset default margin */
+}
+iframe {
+    display: block;       /* iframes are inline by default */
+    background: #000;
+    border: none;         /* Reset default border */
+    height: 100vh;        /* Viewport-relative units */
+    width: 100vw;
+}
+</style>
+<body>
+<iframe src="assets/print/<?php echo $_GET['pdf_file']?>"></iframe>
+</body>
